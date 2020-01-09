b298b6fd7c92148b96c12fee8c7337c61e18848d
Soft-code-hard <57468016+Soft-code-hard@users.noreply.github.com>
2020-01-09 16:19:31 -0500

    Parent/child process
    Added parent/child process form

---
+++ menu_child.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+#Processing variables
+count = 600      #changer à 600 en situation réelle
+start_time=0    #début (timeout)
+elapsed_time=0  #temps écoulé (timeout)
+
+# Fonction qui s'execute lorsque le process child a recu un signal d'arret par le parent
+# Utiliser cette fonction pour faire le menage au besoin...
+function arreter_processus()
+{
+    echo ">> Signal d'interruption du child recu"
+    # Ne pas oublier de sortir du script child a la fin
+    exit
+}
+
+# Cette commande intercepte le signal envoye par la commande "kill" du parent. Lorsque le signal
+# est intercepté, la fonction arreter_processus est appelee.
+trap arreter_processus SIGTERM
+
+
+
+# On releve le ID du parent
+PARENT_PID=$(ps $$ -o ppid=)
+echo ">> Processus child démarre avec PID : $$, le PID du parent $PARENT_PID"
+
+# boucle infinie (pour simuler les releves a partir de tty0
+while ["$elapsed_time" -lt "$count"]; do
+	#Keep track of length of time without an input read
+	READ=`dd if=/dev/ttyUSB0 time = 600 | sed 's/ /*/g'`
+	DATA=$(echo $READ | sed 's/ /,/g')
+	aws kinesis put-record --stream-name MicsaDataStreaming --data $DATA --partition-key data
+	echo "$DATA"
+	#Long-term, we could use the optional --sequence-number-for-ordering parameter, which guarantees proper ordering of outgoing data
+    if [ "$DATA" -eq 0 ] && [ "$elapsed_time" -eq 0 ]; then
+		$start_time=date +%s%N | cut -b1-13
+        $elapsed_time=$(((date +%s%N | cut -b1-13)-$start_time))
+    elif [ "$DATA" -eq 0 ] && [ "$elapsed_time" -gt 0 ]; then
+        $elapsed_time=$(((date +%s%N | cut -b1-13)-$start_time))
+    elif [ "$DATA" -gt 0 ]; then
+        $start_time=0
+        $elapsed_time=0
+	fi
+done
+
+arreter_processus
+
+
+# Supposons que le processus child se termine, si il faut aussi que le parent se termine: 
+# kill $PARENT_PID
---
+++ menu_parent.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+
+# La variable $$ nous donne toujours le PID du processus actuel
+echo "Processus parent demarre avec PID: $$"
+
+
+## FUNCTIONS DEFINITIONS
+#Close application
+function quit {
+    exit
+}
+
+#Open session menu
+function session {
+    select opt in "${sessionOptions[@]}"
+do
+    case $opt in
+        "New Session")
+			aws kinesis create-stream --stream-name MicsaDataStreaming --shard-count 1
+            runSession
+			exit session
+            ;;
+        "Exit Session Menu")
+            echo "Back to main menu"
+            echo "1) Log In"
+            echo "2) Exit Program"
+            return
+            break
+            ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
+}
+
+
+#Begin recording session
+function runSession {
+    #Function read user input continously
+    echo "New session is in progress. Serial data is being recorded."
+	while[1]; do
+		read -n1 -r -p "Press S to start and T to terminate" key
+		if [ "$key" = "s" ]; then
+			# La variable $! nous donne le PID du dernier sous-processus demarre
+			# Le & a la fin de la commande demarre le processus en backgroup (donc en parallele)
+			./child.sh &
+			CHILD_PID=$!
+		elif [ "$key" = "t"];
+			# A la fin du programme il faut tuer le process child. Il ne se tue pas tout seul car le sous-process
+			# qu'on a demarre dans le background n'est pas un sous-processus de parent.sh mais du shell. 
+			kill $CHILD_PID
+			#Stops AWS stream
+			aws kinesis delete-stream --stream-name MicsaDataStreaming
+			#Exit function
+			echo "Recording terminated."
+			exit runSession
+		fi
+	done
+}
+
+
+
+
+## START MAIN PROGRAM 
+echo "Processus child demarre avec PID: $CHILD_PID"
+select opt in "${menuOptions[@]}"
+do
+    case $opt in
+        "Log In")
+            echo "Please enter your username and password (a space between the two): "
+            read existingUSN existingPW
+            if grep -q "$existingUSN $existingPW" users.txt
+            then
+                echo "Successfully authenticated."
+                echo "Session Menu:"
+                session
+            else
+                echo "Invalid Credentials."
+            fi
+            ;;
+        "Exit Program")
+            quit
+            break
+            ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
+
+echo "Execution du parent terminee"
+
+
+
+
+
+
+
