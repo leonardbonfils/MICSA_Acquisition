e8fd93d82ed921cf32415b32c9f7379f91d169e8
EK230948230598 <6782987+EK230948230598@users.noreply.github.com>
2020-03-19 19:25:04 -0400

    Initial draft of Kafka Producer script

--- producer.py
+++ producer.py
@@ -1,18 +1,68 @@
-#!/usr/bin/python
+"""#!/usr/bin/python"""
+#!"C:\Program Files\WindowsApps\PythonSoftwareFoundation.Python.3.8_3.8.752.0_x64__qbz5n2kfra8p0"
+
+# ------------------------------ Script Kafka Producer ------------------------------- #
+# -------------------------- Club SynapsETS - MICSA Project -------------------------- #
+
+# Libraries
 import serial 
-import json
-import time
+from json import dumps
+from time import sleep
 from kafka import KafkaProducer
+from kafka.errors import KafkaError
 
-# Definition des donnees
-ser = serial.Serial('/dev/ttyUSB0', 9600)
-#producer = KafkaProducer(bootstrap_servers='localhost:9062', request_timeout_ms=10000)
-topic = 'micsa_data'
+# Parameters
+#ser = serial.Serial('/dev/ttyUSB0', 9600)
+serverIP = ['10.194.24.26:9092']
+client_id = 'rasPi'
+topic = 'micsaData'
+retries = 5
+request_timeout_ms = 10000
 
+# ------------------------------------------------------------------------------------ #
+# ------------------------------- Producer definition -------------------------------- #
+# ------------------------------------------------------------------------------------ #
+
+# Producer definition - very basic to run our initial tests
+producer = KafkaProducer(\
+    bootstrap_servers=serverIP,\
+    value_serializer=lambda x:dumps(x).encode('utf-8'))
+
+# Producer definition - full definition
+"""
+producer = KafkaProducer(\
+    bootstrap_servers=serverIP,\
+    client_id = client_id,\
+    retries=retries,\
+    value_serializer=lambda x:dumps(x).encode('utf-8'),\
+    request_timeout_ms = request_timeout_ms,\
+    )
+"""
+
+# ------------------------------------------------------------------------------------ #
+# ------------------------------ Producer transmission ------------------------------- #
+# ------------------------------------------------------------------------------------ #
+
+# Producer data transmission - test data
+for e in range(10):
+    data = {'number' : e}
+    producer.send(topic, value=data)
+    sleep(5)
+
+# Producer data transmission - serial data
+"""
 # Boucle
 while True:
     data = ser.readline()
     if data:
         print(data)
         data=None
-        #producer.send(b'')
+        producer.send(b'')
+"""
+
+# Close the producer
+producer.close()
+
+# ------------------------------------------------------------------------------------ #
+# ---------------------------------- End of script ----------------------------------- #
+# ------------------------------------------------------------------------------------ #
