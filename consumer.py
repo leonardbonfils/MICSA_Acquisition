d58e64b9aa7e82707f9e5587dd4b34ed476e8943
EK230948230598 <6782987+EK230948230598@users.noreply.github.com>
2020-03-19 19:15:28 -0400

    Deleted consumer.py Script created by mistake

--- consumer.py
+++
@@ -1,9 +0,0 @@
-from kafka import kafkaConsumer
-from pymongo import MongoClient
-from json import loads
-
-
-consumer = kafkaConsumer('micsa_data',bootstrap_servers=['localhost:9092'],auto_offset_reset ='earliest',enable_auto_commit=1ms, group_id='my-group')
-
-client = MongoClient('localhost:27017')
-collection = client.micsa_data.micsa_data
