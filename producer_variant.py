92519906e3bffad5de1dc8b0c2ec11c5bc20bc10
Jacques <jveneziano@esmartcontrol.com>
2020-07-26 18:39:14 -0400

    Modif to producer variant, added json message with user info

--- producer_variant.py
+++ producer_variant.py
@@ -7,21 +7,31 @@ from time import sleep
 from json import dumps
 from kafka import KafkaProducer
 from serial import Serial
+from datetime import datetime
+from bson import json_util
 
-# Definition des donnees
+# Data definitions
 ser = serial.Serial('/dev/ttyUSB0', 9600)
-producer = KafkaProducer(bootstrap_servers=['10.194.24.26:9092'], value_serializer=lambda x:dumps(x).encode('utf-8'))
+producer = KafkaProducer(bootstrap_servers=['10.194.24.26:9092'],
+						 value_serializer=lambda x:dumps(x).encode('utf-8'))
 topic = 'numtest'
 
+# We send the user's information with the date to create a specific stream
+date = datetime.now()
+date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
+user_dict = {'username': currentuser, 'password': user_password, stream_date:
+				date_time}
 
-# On envoie en premier les infos de usagé et les informations de la série de données: date, heure et secondes.
+producer.send('numtest', json.dumps(user_dict,
+				 default=json_util.default).encode('utf-8'))
 
 
-# Boucle
+# loop
 while True:
     data = ser.readline()
     if data:
         print(data)
         data = re.sub('\r |\n','',data)
-        producer.send('numtest', data_numbers_only)
+        producer.send('numtest', data, 
+        				timestamp_ms = lambda: int(round(time.time()*1000)))
         sleep(2)
